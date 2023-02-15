84455f3ccbe538ec997925a64421cddeb1c30a19
Rafael Fernandes Montes <rafaelmontes92@outlook.com>
2023-02-15 13:54:18 -0300

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobaStyle } from './styles/global'
+
+export function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobaStyle />
+      
+      dt-money
+    </ThemeProvider>
+  )
+}
